c56175279b78d6f328af16c119ff38e86a3c904d
Mahmoud Elattar <mahmoudelattar@Mahmouds-Mac.local>
2021-04-25 23:55:42 -0700

    remove story board and add read me file

---
+++ Instgram-Firebase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instgram-Firebase
+//
+//  Created by Mahmoud Elattar on 4/25/21.
+//  Copyright © 2021 ITI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Instgram-FirebaseTests/Instgram_FirebaseTests.swift
@@ -0,0 +1,34 @@
+//
+//  Instgram_FirebaseTests.swift
+//  Instgram-FirebaseTests
+//
+//  Created by Mahmoud Elattar on 4/25/21.
+//  Copyright © 2021 ITI. All rights reserved.
+//
+
+import XCTest
+@testable import Instgram_Firebase
+
+class Instgram_FirebaseTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
