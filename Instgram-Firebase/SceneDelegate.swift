f1dc7737d1c86d77092142ad3dd53b4a22aadfcc
Mahmoud Elattar <mahmoudelattar@Mahmouds-Mac.local>
2021-05-02 06:28:28 -0700

    first structure of user profile screen

--- Instgram-Firebase/SceneDelegate.swift
+++ Instgram-Firebase/SceneDelegate.swift
@@ -17,13 +17,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let WindowScene = (scene as? UIWindowScene) else { return }
+    guard let WindowScene = (scene as? UIWindowScene) else { return }
         window=UIWindow(frame: WindowScene.screen.bounds)
         window?.windowScene = WindowScene
-        window?.rootViewController  = SignUpVC()
+        //window?.rootViewController  = SignUpVC()
+        window?.rootViewController  = MainTabBarController()
         window?.makeKeyAndVisible()
-        
-        
+    
         
     }
 
---
+++ Instgram-Firebase/ui/reusable_components/MainTabBarController.swift
@@ -0,0 +1,26 @@
+//
+//  MainTabBarController.swift
+//  Instgram-Firebase
+//
+//  Created by Mahmoud Elattar on 5/2/21.
+//  Copyright © 2021 ITI. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController : UITabBarController {
+    
+    override func viewDidLoad() {
+       super.viewDidLoad()
+        view.backgroundColor = .red
+        
+        let userProfileVC = UserProfileVC(collectionViewLayout: UICollectionViewLayout())
+        let navController = UINavigationController(rootViewController: userProfileVC)
+        let vc1 = UIViewController()
+        vc1.view.backgroundColor = .green
+        // UITabBar.appearance().tintColor = .black
+//tabBar.tintColor = .black
+        
+        self.viewControllers = [navController , vc1]
+    }
+}
