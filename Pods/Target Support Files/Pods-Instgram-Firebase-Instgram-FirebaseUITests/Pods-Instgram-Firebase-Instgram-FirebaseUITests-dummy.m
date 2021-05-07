95122afea65bbc0fa325d10d8ec4ca5f6dbcf88c
Mahmoud Elattar <mahmoudelattar@Mahmouds-Mac.local>
2021-05-07 14:30:08 -0700

    switched

---
+++ Pods/Target Support Files/Pods-Instgram-Firebase-Instgram-FirebaseUITests/Pods-Instgram-Firebase-Instgram-FirebaseUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Instgram_Firebase_Instgram_FirebaseUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Instgram_Firebase_Instgram_FirebaseUITests
+@end
---
+++ Pods/Target Support Files/Pods-Instgram-FirebaseTests/Pods-Instgram-FirebaseTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Instgram_FirebaseTests : NSObject
+@end
+@implementation PodsDummy_Pods_Instgram_FirebaseTests
+@end
